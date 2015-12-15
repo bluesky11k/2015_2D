467543dae0571d2bd94bdcf3c96f42a4e6a5e0f4
KimSeunghyeon <loryk3737@naver.com>
2015-12-15 21:34:22 +0900

    frame_time 오류수정 및 총알발사 수정

--- project_K/background.py
+++ project_K/background.py
@@ -1,38 +1,30 @@
-#__author__ = 'KimSeunghyeon'
-import random
 
+import random
 from pico2d import *
 
-#
-# class Background:
-#     # PIXEL_PER_METER = (10.0 / 0.3)           # (10.0 / 0.3) = 10 pixel 30 cm
-#     # SCROLL_SPEED_KMPH = 100.0                    # Km / Hour
-#     # SCROLL_SPEED_MPM = (SCROLL_SPEED_KMPH * 1000.0 / 60.0)
-#     # SCROLL_SPEED_MPS = (SCROLL_SPEED_MPM / 60.0)
-#     # SCROLL_SPEED_PPS = (SCROLL_SPEED_MPS * PIXEL_PER_METER)
-#
-#     def __init__(self, w, h):
-#         self.image = load_image('resource/background02.png')
-#         self.up = 0
-#         self.bottom = 0
-#         self.screen_width = w
-#         self.screen_height = h
-#
-#     def get_bb(self):
-#         pass
-#
-#     def draw(self):
-#         y = int (self.up)
-#         h = min (self.image.h - y, self.screen_height)
-#         self.image.clip_draw_to_origin (0, y, self.screen_width, h, 0, 0)
-#         self.image.clip_draw_to_origin (0, 0, self.screen_width, self.screen_height - h, 0, h)
-#
-#     def update(self, frame_time):
-#         self.up = (self.up + frame_time * self.speed) % self.image.h
-#
-#     def draw_bb(self):
-#         pass
-#
-#     def stop(self):
-#         return
-#
+
+class Background:
+    MOVE_PER_SEC = 100
+
+    def __init__(self):
+        self.image = load_image('resource/background.png')
+
+        self.bgm = load_music('resource/backgruoundmusic.mp3')
+        self.bgm.set_volume(64)
+        self.bgm.repeat_play()
+
+        self.y = 0
+
+
+    def update(self, frame_time):
+        speed = frame_time * self.MOVE_PER_SEC
+        self.y += speed
+        if self.y >= 800:
+            self.y = 0
+
+    def draw(self): # 2800 3840
+        self.image.clip_draw
+        self.image.clip_draw(0, int(self.y), 1400, int(1920-self.y)+1, 700, int((1920 - self.y)/2))
+
+
+
